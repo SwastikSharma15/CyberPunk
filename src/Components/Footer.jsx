5661caa4934c472223b892f767a83f739b35c06b
Swastik <sharmaspeedx29@gmail.com>
2025-11-08 15:44:53 +0530

    footer section

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { FaDiscord, FaGithub, FaTwitter } from 'react-icons/fa'
+import { FaTwitch } from 'react-icons/fa6'
+
+const links = [
+  {href: 'https://discord.com', icon: <FaDiscord />},
+  {href: 'https://twich.com', icon: <FaTwitch />},
+  {href: 'https://twitter.com', icon: <FaTwitter />},
+  {href: 'https://github.com', icon: <FaGithub />},
+]
+
+const Footer = () => {
+  return (
+    <footer className='w-screen bg-violet-300 py-4 text-black' >
+      <div className='container m-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row'>
+        <p className='text-center text-sm md:text-left'>
+          &copy; Cyberpunk Edgerunners. All rights reserved
+        </p>
+        <div className='flex justify-center gap-4 md:justify-start'>
+          {links.map((link, index) => (
+            <a key={index} href={link.href} target='_blank' rel='noopener noreferrer' className='text-black transition-colors duration-500 ease-in-out hover:text-white'>
+              {link.icon}
+            </a>
+          ))}
+        </div>
+
+        <a href='#privacy-policy' className='text-center text-sm hover:underline md:text-right'>
+          Privacy Policy
+        </a>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
