625a8c9e238f4a8c4905a4648ea9e23cb948f5ca
Swastik <sharmaspeedx29@gmail.com>
2025-11-23 00:52:02 +0530

    update

--- src/Components/Story.jsx
+++ src/Components/Story.jsx
@@ -47,11 +47,11 @@ const Story = () => {
     <section className="min-h-dvh w-screen bg-black text-blue-50">
 			<div className="flex size-full flex-col items-center py-10 pb-24">
         <p className="font-general text-sm uppercase md:text-[10px]">
-          the multiversal ip world
+          the city that takes everything
         </p>
 				<div className="relative size-full">
           <AnimatedTitle
-            title="the st<b>o</b>ry of <br /> a hidden real<b>m</b>"
+            title="There are no <br /> happy endings <br /> in Night City"
             containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10"
           />
 					<div className="story-img-container ">
@@ -59,9 +59,9 @@ const Story = () => {
 							<div className="story-img-content">
 								<img 
 									ref={frameRef}
-									src="/img/entrance.webp" 
+									src="/img/entrance.jpg" 
 									alt="entrance"
-									className="object-contain"
+									className="object-contain ml-10"
 									onMouseLeave={handelMouseLeave}
 									onMouseUp={handelMouseLeave}
 									onMouseEnter={handelMouseLeave}
@@ -74,8 +74,19 @@ const Story = () => {
 				</div>
 				<div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
 					<div className="flex h-full w-fit flex-col items-center md:items-start">
-						<p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe facere facilis commodi autem vitae dolor mollitia ab tempora eligendi impedit ex optio amet ducimus rem, ipsam quisquam dolore suscipit incidunt.</p>
-						<Button id="realm-button" title="discover prologue" containerClass="mt-5" />
+						<p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">
+							Edgerunners tells a standalone, 10-episode story about a street kid — David — trying to survive in Night City, a technology and body modification-obsessed city of the future. Having everything to lose, he chooses to stay alive by becoming an edgerunner—a mercenary outlaw also known as a cyberpunk.
+						</p>
+						<a
+              href="https://cyberpunk.fandom.com/wiki/Cyberpunk:_Edgerunners"
+              target="_blank"
+            >
+              <Button
+                id="realm-button"
+                title="discover prologue"
+                containerClass="mt-5"
+              />
+            </a>
 					</div>
 				</div>
 			</div>
