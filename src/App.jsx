09078b9e23534fac0f2a9e6f32a3692a831c7958
Swastik <sharmaspeedx29@gmail.com>
2025-11-23 01:17:44 +0530

    performance updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
 import Hero from './Components/Hero'
 import About from './Components/About'
 import NavBar from './Components/NavBar'
@@ -7,7 +9,35 @@ import Story from './Components/Story'
 import Contact from './Components/Contact'
 import Footer from './Components/Footer'
 
+gsap.registerPlugin(ScrollTrigger)
+
 const App = () => {
+  useEffect(() => {
+    // Refresh ScrollTrigger when window is fully loaded
+    const handleLoad = () => {
+      ScrollTrigger.refresh()
+    }
+    
+    window.addEventListener('load', handleLoad)
+
+    // Also refresh on resize (debounced)
+    let resizeTimer
+    const handleResize = () => {
+      clearTimeout(resizeTimer)
+      resizeTimer = setTimeout(() => {
+        ScrollTrigger.refresh()
+      }, 250)
+    }
+    
+    window.addEventListener('resize', handleResize)
+
+    // Cleanup
+    return () => {
+      window.removeEventListener('load', handleLoad)
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
       <NavBar />
