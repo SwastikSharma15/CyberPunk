2e63832203b95aae595164518d4182e394de852f
SwastikSharma15 <sharmaspeedx29@gmail.com>
2026-07-05 15:26:28 +0530

    img optimizations

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -16,18 +16,18 @@ const Contact = () => {
       <div className="relative rounded-lg bg-black py-24 -mx-4 sm:mx-0 text-blue-50 sm:overflow-hidden">
         <div className="absolute -left-20 top-0 hidden h-full w-72 overflow-hidden sm:block lg:left-20 lg:w-96">
           <ImageClipBox
-            src="/img/contact-1.png"
+            src="/img/contact-1.webp"
             clipClass="contact-clip-path-1 scale-90"
           />
           <ImageClipBox
-            src="/img/contact-2.jpg"
+            src="/img/contact-2.webp"
             clipClass="contact-clip-path-2 -translate-y-20 scale-120"
           />
         </div>
         <div className="absolute -top-20 left-20 w-60 sm:top-1/2 md:left-auto md:right-10 lg:top-20 lg:w-80">
           <ImageClipBox
             clipClass="sword-man-clip-path md:scale-135"
-            src="img/swordman.png"
+            src="img/swordman.webp"
           />
         </div>
 
@@ -39,8 +39,8 @@ const Contact = () => {
             title="I<b>n</b>spired by <br /> Cyberpunk Edgerunners <br /> Designed Like Zentry"
             containerClass="--font-family-zentry !md:text-[6.2rem] w-full  text-5xl leading-[0.9] md:text-[6rem] special-font"
           />
-          <Button 
-            title='Made with Love for Lucy' 
+          <Button
+            title='Made with Love for Lucy'
             containerClass='mt-10 cursor-pointer' />
         </div>
       </div>
--- src/Components/Story.jsx
+++ src/Components/Story.jsx
@@ -6,78 +6,78 @@ import Button from "./Button";
 
 const Story = () => {
 
-	const frameRef = useRef('null');
-	const handelMouseLeave = () => {
-		const element = frameRef.current;
+  const frameRef = useRef('null');
+  const handelMouseLeave = () => {
+    const element = frameRef.current;
 
-		gsap.to(element, {
-			duration: 0.3,
-			rotateX: 0,
-			rotateY: 0,
-			ease: 'power1.inOut'
-		})
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX: 0,
+      rotateY: 0,
+      ease: 'power1.inOut'
+    })
 
-	}
+  }
 
-	const handelMouseMove = (e) => {
-		const {clientX, clientY} = e;
-		const element = frameRef.current;
+  const handelMouseMove = (e) => {
+    const { clientX, clientY } = e;
+    const element = frameRef.current;
 
-		if(!e) return;
+    if (!e) return;
 
-		const rect = element.getBoundingClientRect();
-		const x = clientX - rect.left;
-		const y = clientY - rect.top;
+    const rect = element.getBoundingClientRect();
+    const x = clientX - rect.left;
+    const y = clientY - rect.top;
 
-		const centerX = rect.width /2;
-		const centerY = rect.height / 2;
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
 
-		const rotateX = ((y-centerY) / centerY) * -10;
-		const rotateY = ((x-centerX) / centerX) * 10;
+    const rotateX = ((y - centerY) / centerY) * -10;
+    const rotateY = ((x - centerX) / centerX) * 10;
 
-		gsap.to(element, {
-			duration: 0.3,
-			rotateX, rotateY,
-			transformPerspective: 500,
-			ease: 'power1.inOut'
-		})
-	}
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX, rotateY,
+      transformPerspective: 500,
+      ease: 'power1.inOut'
+    })
+  }
 
   return (
     <section className="min-h-dvh w-screen bg-black text-blue-50">
-			<div className="flex size-full flex-col items-center py-10 pb-24">
+      <div className="flex size-full flex-col items-center py-10 pb-24">
         <p className="font-general text-sm uppercase md:text-[10px]">
           the city that takes everything
         </p>
-				<div className="relative size-full">
+        <div className="relative size-full">
           <AnimatedTitle
             title="T<b>h</b>ere are <b>n</b>o <br /> happy e<b>n</b>dings <br /> in <b>n</b>ight City"
             containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10 special-font"
           />
-					<div className="story-img-container ">
-						<div className="story-img-mask">
-							<div className="story-img-content">
-								<img 
-									ref={frameRef}
-									src="/img/entrance.jpg" 
-									alt="entrance"
-									className="object-contain ml-10"
-									onMouseLeave={handelMouseLeave}
-									onMouseUp={handelMouseLeave}
-									onMouseEnter={handelMouseLeave}
-									onMouseMove={handelMouseMove}
-									/>
-							</div>
-						</div>
-						<RoundedCorners />
-					</div>
-				</div>
-				<div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
-					<div className="flex h-full w-fit flex-col items-center md:items-start">
-						<p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">
-							Edgerunners tells a standalone, 10-episode story about a street kid — David — trying to survive in Night City, a technology and body modification-obsessed city of the future. Having everything to lose, he chooses to stay alive by becoming an edgerunner—a mercenary outlaw also known as a cyberpunk.
-						</p>
-						<a
+          <div className="story-img-container ">
+            <div className="story-img-mask">
+              <div className="story-img-content">
+                <img
+                  ref={frameRef}
+                  src="/img/entrance.webp"
+                  alt="entrance"
+                  className="object-contain ml-10"
+                  onMouseLeave={handelMouseLeave}
+                  onMouseUp={handelMouseLeave}
+                  onMouseEnter={handelMouseLeave}
+                  onMouseMove={handelMouseMove}
+                />
+              </div>
+            </div>
+            <RoundedCorners />
+          </div>
+        </div>
+        <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
+          <div className="flex h-full w-fit flex-col items-center md:items-start">
+            <p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">
+              Edgerunners tells a standalone, 10-episode story about a street kid — David — trying to survive in Night City, a technology and body modification-obsessed city of the future. Having everything to lose, he chooses to stay alive by becoming an edgerunner—a mercenary outlaw also known as a cyberpunk.
+            </p>
+            <a
               href="https://cyberpunk.fandom.com/wiki/Cyberpunk:_Edgerunners"
               target="_blank"
             >
@@ -87,10 +87,10 @@ const Story = () => {
                 containerClass="mt-5"
               />
             </a>
-					</div>
-				</div>
-			</div>
-		</section>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
